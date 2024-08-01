d27e5c2fdf50ee49146d9c2dcdd563315fbb567d
letUser <work.sotnik@gmail.com>
2024-08-01 09:30:26 -0400

    API gateway DNS fix

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,12 @@ import 'element-plus/theme-chalk/src/message.scss'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
+
+axios.defaults.baseURL = 'https://d5dt6p5vcdddtv25ckhs.apigw.yandexcloud.net'
